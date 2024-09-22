3a23696d80f193adfd82a2eb6e5c9462a5908a2d
pedrolodonio <pedrolodonio20@gmail.com>
2024-09-22 19:36:34 -0300

    altera classe CityProcessor:adiciona o processamento de dados na cidade,adiciona leitura de arquivos csv, adiciona a impressão dos dados mensais lidos

--- atividade_coletiva/src/Main.java
+++ atividade_coletiva/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        
         int[] threadConfigurations = {1, 2, 4, 8, 16, 32, 64, 80, 160, 320};
         Experiment experiment = new Experiment(1, false);
         experiment.runExperimentsWithDifferentThreads(threadConfigurations);
 
         Experiment experimentWithYearThreads = new Experiment(1, true);
         experimentWithYearThreads.runExperimentsWithDifferentThreads(threadConfigurations);
+
+        
     }
 }
